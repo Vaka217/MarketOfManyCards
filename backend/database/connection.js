ca2ceef0e5e400493495941f6c76180b54e26f41
NMenendez24 <nahuelmt24@gmail.com>
2023-06-17 17:55:16 -0300

    Change the connection.js to use a Password from the environment

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,8 +1,7 @@
-// database/connection.js
 const { Sequelize } = require('sequelize');
 
 // Configura la conexión a la base de datos MySQL
-const sequelize = new Sequelize('cardmarket', 'root', 'root', {
+const sequelize = new Sequelize('cardmarket', 'root', process.env.MYSQL_ROOT_PASSWORD, {
   host: 'db',
   dialect: 'mysql',
 });
