352b1d757df277fa8f11aaee3280d53ece626f28
NMenendez24 <nahuelmt24@gmail.com>
2023-06-21 12:56:16 -0300

    Change the connection to environment variables

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,7 +1,8 @@
+// database/connection.js
 const { Sequelize } = require('sequelize');
 
 // Configura la conexión a la base de datos MySQL
-const sequelize = new Sequelize('cardmarket', 'root', process.env.MYSQL_ROOT_PASSWORD, {
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_ROOT_PASSWORD, {
   host: 'db',
   dialect: 'mysql',
 });
