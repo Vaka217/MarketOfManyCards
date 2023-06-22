9f923649bd163974319ccfe9a991d186642796cb
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-06-22 10:19:56 -0300

    Add nickname and contact for creating User in signup route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,46 +1,46 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
-  const { email, password } = req.body;
+router.post("/signup", async (req, res) => {
+  const { email, password, nickname, contact } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.create({ email, password, nickname, contact });
 
-    const token = jwt.sign({ userId: user.id }, 'MY_SECRET_KEY');
+    const token = jwt.sign({ userId: user.id }, "MY_SECRET_KEY");
     res.send({ token });
   } catch (err) {
     return res.status(422).send(err.message);
   }
 });
 
-router.post('/signin', async (req, res) => {
+router.post("/signin", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(422).send({ error: 'Must provide email and password' });
+    return res.status(422).send({ error: "Must provide email and password" });
   }
 
   try {
     const user = await User.findOne({ where: { email } });
 
     if (!user) {
-      return res.status(422).send({ error: 'Invalid password or email' });
+      return res.status(422).send({ error: "Invalid password or email" });
     }
 
     const isMatch = await user.comparePassword(password);
 
     if (!isMatch) {
-      return res.status(422).send({ error: 'Invalid password or email' });
+      return res.status(422).send({ error: "Invalid password or email" });
     }
 
-    const token = jwt.sign({ userId: user.id }, 'MY_SECRET_KEY');
+    const token = jwt.sign({ userId: user.id }, "MY_SECRET_KEY");
     res.send({ token });
   } catch (err) {
-    return res.status(422).send({ error: 'Invalid password or email' });
+    return res.status(422).send({ error: "Invalid password or email" });
   }
 });
 
