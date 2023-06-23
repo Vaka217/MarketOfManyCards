1ff22b5e3b26dd56a619f7ecb0779b6ed1dae968
NMenendez24 <nahuelmt24@gmail.com>
2023-06-23 12:35:02 -0300

    Change the docker-compose and workflow to work with secrets

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -2,8 +2,8 @@
 const { Sequelize } = require('sequelize');
 
 // Configura la conexión a la base de datos MySQL
-const sequelize = new Sequelize('cardmarket', 'root', 'root', {
-  host: 'db',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
 });
 
